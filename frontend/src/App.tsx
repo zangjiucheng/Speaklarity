256546a2402494489ba2cbfd57eca77ca7d11375
Alex Xu <alex.zihao.xu@gmail.com>
2025-07-19 16:07:33 -0400

    Init Commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+import './index.css';
+
+function App() {
+    
+    return (
+        <>
+            <div className="">
+                hi mom
+            </div>
+        </>
+    );
+}
+
+export default App;
